4e1a5639df067f1ade8a80fb34b10bba08f76bb6
Hussein Guevara <80933892+HusseinGuevara@users.noreply.github.com>
2021-11-29 16:24:24 -0800

    Add files via upload

---
+++ BinaryTree.js
@@ -0,0 +1,55 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+    insert(value) {
+        var newNode = new Node(value);
+        if(this.root === null) {
+            this.root = newNode;
+            return this;
+        } else {
+            var current = this.root;
+            while(true) {
+                if(value === current.value) return undefined;
+                if(value < current.value) {
+                    if(current.left === null) {
+                        current.left = newNode;
+                        return this;
+                    } else {
+                        current = current.left;
+                    }
+                } else if(value > current.value) {
+                    if(current.right === null) {
+                        current.right = newNode;
+                        return this;
+                    } else {
+                        current = current.right;
+                    }
+                }
+            }
+        }
+        
+    }
+}
+
+var tree = new BinarySearchTree();
+tree.insert(5);
+tree.insert(4);
+tree.insert(6);
+tree.insert(2);
+tree.insert(8);
+tree.insert(7);
+console.log(tree);
+
+
+
+
+
